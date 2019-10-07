19cc8da2e1c0c732a576b525d1f6d7fdf71aa456
jacobnewby <jacobnewby01@gmail.com>
2019-10-07 16:12:53 -0400

    update

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,104 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Data.Entity;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDbContext _dB = new RestaurantDbContext();
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View(_dB.Restaurants.ToList());
+        }
+        //GET: Restaurant/Create
+        public ActionResult Create()
+        { 
+            return View();
+        }
+        //POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if(ModelState.IsValid)
+            {
+                _dB.Restaurants.Add(restaurant);
+                _dB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+        //GET: restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _dB.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant); 
+        }
+        //POST: Restaurant/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _dB.Restaurants.Find(id);
+            _dB.Restaurants.Remove(restaurant);
+            _dB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //GET: Restaurant/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _dB.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        //POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _dB.Entry(restaurant).State = EntityState.Modified;
+                _dB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+        //GET: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _dB.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+    }
+}
